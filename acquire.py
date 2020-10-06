6e099972309f976628223e647acd9dc101832730
Adam Gomez <adam.daniel.gomez.787@gmail.com>
2020-10-06 16:37:46 -0500

    Added acquire tax relevant dataframe function

--- acquire.py
+++ acquire.py
@@ -38,3 +38,21 @@ def acquire_only(query):
     zillow = pd.read_sql(query, url, index_col='id')
 #   zillow['County'] = zillow.apply(lambda row: add_county_column(row), axis = 1)
     return zillow
+
+def acquire_cache_tax_data():
+    if not path.isfile('zillow_tax_rate.csv'):
+        query = '''
+        SELECT p.fips, ROUND((p.taxamount / p.taxvaluedollarcnt) * 100, 2) AS taxrate
+        FROM properties_2017 AS p
+        JOIN predictions_2017 AS pr USING (parcelid) 
+        WHERE p.propertylandusetypeid IN (261, 262, 263, 264, 266, 268, 273, 275, 276, 279)
+        AND pr.transactiondate between '2017-05-01' AND '2017-06-30'
+        '''
+        url = get_connection('zillow')
+        zillow = pd.read_sql(query, url)
+        zillow.to_csv('zillow_tax_rate.csv')
+    zillow = pd.read_csv('zillow_tax_rate.csv')
+    zillow.drop(columns=['Unnamed: 0'], inplace=True)
+    zillow['County'] = zillow.apply(lambda row: add_county_column(row), axis = 1)
+    zillow.drop(columns=['fips'], inplace=True)
+    return zillow
